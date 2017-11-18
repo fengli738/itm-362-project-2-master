f62923fb4d9a9b96d8afc0e3d0a01cc7034e8705
Matteo Giaimo <mgiaimo@hawk.iit.edu>
2017-11-17 18:17:14 -0600

    added java script
    i added java script to make the image responsive but i am still testing it to make sure it benefits the user

--- java/java.js
+++ java/java.js
@@ -1,17 +1,8 @@
-$(function() {
-  $( "button" ).click(function() {
-    $( "button" ).addClass( "onclic", 250, validate);
-  });
+function info() 
+{
+    document.getElementById('myimage').src = "project-2-images/back2.jpg";
+}
 
-  function validate() {
-    setTimeout(function() {
-      $( "button" ).removeClass( "onclic" );
-      $( "button" ).addClass( "validate", 450, callback );
-    }, 2250 );
-  }
-    function callback() {
-      setTimeout(function() {
-        $( "button" ).removeClass( "validate" );
-      }, 1250 );
-    }
-  });
+function stoplogo() {
+    document.getElementById('myimage').src = "project-2-images/onestop.jpg";
+}
